355c2f64be674fc6acdb96e4158fc017b17f24de
Jhonatan Felipe <scarpa.jhonatan@outlook.com>
2023-04-14 18:15:41 -0300

    config prisma and register user start

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -6,6 +6,7 @@ import { z } from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
+import { api } from '@/lib/axios'
 
 const registerFormSchema = z.object({
   username: z
@@ -40,7 +41,17 @@ export default function Register() {
     }
   }, [router.query?.username, setValue])
 
-  async function handleRegister(data: RegisterFormData) {}
+  async function handleRegister(data: RegisterFormData) {
+    const { name, username } = data
+    try {
+      await api.post('/users', {
+        name,
+        username,
+      })
+    } catch (err) {
+      console.log(err)
+    }
+  }
 
   return (
     <Container>
