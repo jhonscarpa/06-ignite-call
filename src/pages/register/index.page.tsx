66a4147cd62d75da8612f9e2fe4be96eae9d44b4
Jhonatan Felipe <scarpa.jhonatan@outlook.com>
2023-04-14 18:43:45 -0300

    cookie

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -7,6 +7,7 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
 import { api } from '@/lib/axios'
+import { AxiosError } from 'axios'
 
 const registerFormSchema = z.object({
   username: z
@@ -49,7 +50,12 @@ export default function Register() {
         username,
       })
     } catch (err) {
-      console.log(err)
+      if (err instanceof AxiosError && err?.response?.data?.message) {
+        alert(err.response.data.message)
+        return
+      }
+
+      console.error(err)
     }
   }
 
